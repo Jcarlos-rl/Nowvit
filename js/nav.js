49ae1e352d8ff1f61d5c1ef5bda94c6942c722d0
Jcarlos-rl <carlos_-619@hotmail.com>
2018-07-22 19:04:07 -0500

    Modificaciones v2

--- js/nav.js
+++ js/nav.js
@@ -7,25 +7,25 @@ $(document).ready(function(){
 
 	$(window).scroll(function(){
         scroll = $(window).scrollTop();
-        if(scroll > 30){
+        if(scroll > 60){
 			if(!flag){
                 if(x.matches){
                     $("#logo").css({"margin-top":"1px", "width":"45px"});
                 }else{
                     $("#logo").css({"margin-top":"3px", "width":"50px"});
                 }
-                $(".center1").css({"visibility":"hidden"});
+                $("nav").css({"background-color":"#0d1925"});
 
 				flag = true;
 			}
 		}else{
 			if(flag){
                 if(x.matches){
-                    $("#logo").css({"margin-top":"120px", "width":"150px"});
+                    $("#logo").css({"margin-top":"100px", "width":"200px"});
                 }else{
-                $("#logo").css({"margin-top":"120px", "width":"250px"});
+                $("#logo").css({"margin-top":"130px", "width":"300px"});
                 }
-                $(".center1").css({"visibility":"visible"});
+                $("nav").css({"background-color":"transparent"});
 
 				flag = false;
 			}
